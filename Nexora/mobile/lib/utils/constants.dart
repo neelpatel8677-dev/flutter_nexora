1894575f93943855b9a3c693841d319e9783ecd3
neelpatel8677-dev <neelpatel8677@gmail.com>
2026-09-25 15:29:02 +0530

    Update mobile API base URL
    Point the Flutter client at the deployed flutter-nexora Render API endpoint.

--- Nexora/mobile/lib/utils/constants.dart
+++ Nexora/mobile/lib/utils/constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   // Change this to your backend URL
   // For Android emulator use: http://10.0.2.2:5000
   // For real device use your computer's local IP: http://192.168.x.x:5000
-  static const String baseUrl = 'https://nexorabackend-1cgs.onrender.com/api';
+  static const String baseUrl = 'https://flutter-nexora.onrender.com/api';
 
   static const String tokenKey = 'nexora_token';
   static const String userKey = 'nexora_user';
